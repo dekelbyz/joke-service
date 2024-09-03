93140ccac140fcdc1c79329d5aef98a4a93f9344
dekelbyz <bayazi455@gmail.com>
2024-09-03 17:39:55 +0300

    Drop init_db approach

--- queue_consumer/init_db.py
+++
@@ -1,8 +0,0 @@
-from models import Base, engine
-
-def initialize_db():
-    print("Creating the database tables if they don't exist...")
-    Base.metadata.create_all(bind=engine)
-    print("Database initialized successfully.")
-
-
