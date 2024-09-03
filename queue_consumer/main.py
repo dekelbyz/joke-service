a1af9f5ad32b350bfeb45e8ddd398df6e1d9961e
dekelbyz <bayazi455@gmail.com>
2024-09-03 18:04:27 +0300

    Final touches consumer - documentations and typo fixing

--- queue_consumer/main.py
+++ queue_consumer/main.py
@@ -3,6 +3,16 @@ from consumer import RabbitMQConsumer
 import consts
 
 def main():
+    '''
+    Main function
+    Orchestrates the entire consuming & DB writing process
+    Order of business:
+
+    - Instantiates DB Handler - which initializes DB on its constructor
+    - Instantiates RabbitMQ consumer class - which is in charge of the consuming process
+    * Note - the RabbitMQ consumer also triggers the DB Writing process, that's why he's receiving 
+      the db_handler instance as an argument.
+    '''
     db_handler = DatabaseHandler()
 
     rabbitmq_consumer = RabbitMQConsumer(
@@ -11,7 +21,10 @@ def main():
         user=consts.RABBITMQ_USER,
         password=consts.RABBITMQ_PASSWORD,
         queue_name=consts.RABBITMQ_JOKE_METADATA_QUEUE,
-        db_handler=db_handler  # Pass the handler to the consumer (dependency injection)
+
+        # RabbitMQConsumer is also in charge of triggering the DB write process. 
+        # That's why he's getting the db_handler as a parameter. I chose dependency injection in this case.
+        db_handler=db_handler 
     )
 
     rabbitmq_consumer.connect()
