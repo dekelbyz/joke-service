2abac94b62624341b7236e1a7e8d08469b4dfd8e
dekelbyz <bayazi455@gmail.com>
2024-09-04 13:51:32 +0300

    Transition to singleton approach to avoid multiple connections

--- queue_publisher_middleware/middleware.py
+++ queue_publisher_middleware/middleware.py
@@ -3,38 +3,31 @@ from starlette.requests import Request
 from fastapi import FastAPI
 from datetime import datetime
 import json
-from queue_publisher_middleware.queue_handler import send_to_rabbitmq
+from queue_publisher_middleware.queue_handler import rabbit_mq_handler
 from queue_publisher_middleware import consts
 
 
-file = open(consts.JSON_ACCOUNTS_FILE_NAME)
-accounts = json.load(file)
+with open(consts.JSON_ACCOUNTS_FILE_NAME, "r") as file:
+    accounts = json.load(file)
+
 
 class PublisherMiddleware(BaseHTTPMiddleware):
     '''
-    PublisherMiddleware is a middleware class 
-    In charge of extracting specific metadata for every incoming request
-    And trigger the send_to_rabbitmq function
-
-    - We use starlette for the middleware behavior by inheriting from its BaseHTTPMiddleware class
-    - We trigger the send_to_rabbitmq function that writes the data to rabbitMQ
     '''
 
     async def dispatch(self, request: Request, call_next):
         account = dict(request.headers).get('authorization', 'NaN')
         response = await call_next(request)
 
-        send_to_rabbitmq(
+        rabbit_mq_handler.publish(
             message={
-            'timestamp': datetime.utcnow().isoformat(),
+            'timestamp': datetime.now().isoformat(),
             'account': 'UNAUTHORIZED' if account not in accounts else account,
             'client_ip': request.client.host,
             'endpoint': request.url.path,
             'method': request.method,
-            'status_code':  int(response.status_code), 
-        },
-        queue=consts.RABBITMQ_JOKE_METADATA_QUEUE
-        )
+            'status_code': int(response.status_code), 
+        })
         
         return response
 
--- queue_publisher_middleware/queue_handler.py
+++ queue_publisher_middleware/queue_handler.py
@@ -1,34 +1,47 @@
 from queue_publisher_middleware.model import EventDetails
-import pika
+import pika  # type: ignore
 import json
 from queue_publisher_middleware import consts
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
-def send_to_rabbitmq(queue:str, message: EventDetails):
-    '''
-    Publishes an event to rabbitMQ
-    * Configurable to work with any RabbitMQ queue 
-    :param message: EventDetails 
-    :param queue: the desired queue name
-    '''
-    try: 
-        credentials = pika.PlainCredentials(consts.RABBITMQ_USER,consts.RABBITMQ_PASSWORD)
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=consts.RABBITMQ_HOST, credentials=credentials))
-        channel = connection.channel()
-        
-        channel.queue_declare(queue=queue)
-        
-        channel.basic_publish(
-            exchange='',
-            routing_key=queue,
-            body=json.dumps(message)
+class _RabbitMQHandler:
+    def __init__(self, queue):
+        self.credentials = pika.PlainCredentials(
+            consts.RABBITMQ_USER, consts.RABBITMQ_PASSWORD
         )
-    except Exception as e:
-        logging.error('Failed to publish to queue:', e)
-        raise e
-    
-    finally:
-        connection.close()
+
+        self.connection_parameters = pika.ConnectionParameters(
+            host=consts.RABBITMQ_HOST, credentials=self.credentials
+        )
+        self.queue = queue
+        self.connection = None
+        self._channel = None
+
+    @property
+    def channel(self):
+        self._connect()
+        return self._channel
+
+    def _connect(self) -> None:
+        if not self.connection or self.connection.is_closed:
+            self.connection = pika.BlockingConnection(self.connection_parameters)
+            self._channel = self.connection.channel()
+            self._channel.queue_declare(queue=self.queue)
+
+    def publish(self, message: EventDetails):
+        try:
+            self.channel.basic_publish(
+                exchange="", routing_key=self.queue, body=json.dumps(message)
+            )
+
+        except Exception as e:
+            logging.error("Failed to publish to queue:", e)
+            raise e
+
+
+rabbit_mq_handler = _RabbitMQHandler(queue=consts.RABBITMQ_JOKE_METADATA_QUEUE)
